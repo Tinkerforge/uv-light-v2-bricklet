d5820d83276e2c5012a6c4b28c3d5090e6e3432c
Erik Fleckstein <erik@tinkerforge.com>
2018-11-07 18:32:05 +0100

    rust: Rename callback receivers.

--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -12,10 +12,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
                                           // Don't use device before ipcon is connected.
 
-    // Create receiver for UV index events.
-    let uvi_receiver = uvl.get_uvi_receiver();
+    let uvi_receiver = uvl.get_uvi_callback_receiver();
 
-    // Spawn thread to handle received events. This thread ends when the `uvl` object
+    // Spawn thread to handle received callback messages.
+    // This thread ends when the `uvl` object
     // is dropped, so there is no need for manual cleanup.
     thread::spawn(move || {
         for uvi in uvi_receiver {
