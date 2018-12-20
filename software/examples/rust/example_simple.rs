8aa1a2b5d6333bb11da5f6c620d4b570c6479f86
Erik Fleckstein <erik@tinkerforge.com>
2018-12-20 16:42:10 +0100

    rust: Reformat examples.

--- software/examples/rust/example_simple.rs
+++ software/examples/rust/example_simple.rs
@@ -1,6 +1,8 @@
-use std::{error::Error, io};
+use std::{io, error::Error};
 
-use tinkerforge::{ip_connection::IpConnection, uv_light_v2_bricklet::*};
+use tinkerforge::{ip_connection::IpConnection, 
+                  uv_light_v2_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -11,19 +13,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     let uvl = UvLightV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    // Get current UV-A.
-    let uva = uvl.get_uva().recv()?;
-    println!("UV-A: {} mW/m²", uva as f32 / 10.0);
+		// Get current UV-A.
+let uva = uvl.get_uva().recv()?;
+		println!("UV-A: {} mW/m²", uva as f32 /10.0);
 
-    // Get current UV-B.
-    let uvb = uvl.get_uvb().recv()?;
-    println!("UV-B: {} mW/m²", uvb as f32 / 10.0);
+		// Get current UV-B.
+let uvb = uvl.get_uvb().recv()?;
+		println!("UV-B: {} mW/m²", uvb as f32 /10.0);
 
-    // Get current UV index.
-    let uvi = uvl.get_uvi().recv()?;
-    println!("UV Index: {}", uvi as f32 / 10.0);
+		// Get current UV index.
+let uvi = uvl.get_uvi().recv()?;
+		println!("UV Index: {}", uvi as f32 /10.0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
--- software/examples/rust/example_threshold.rs
+++ software/examples/rust/example_threshold.rs
@@ -1,5 +1,8 @@
-use std::{error::Error, io, thread};
-use tinkerforge::{ip_connection::IpConnection, uv_light_v2_bricklet::*};
+use std::{io, error::Error};
+use std::thread;
+use tinkerforge::{ip_connection::IpConnection, 
+                  uv_light_v2_bricklet::*};
+
 
 const HOST: &str = "localhost";
 const PORT: u16 = 4223;
@@ -10,23 +13,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     let uvl = UvLightV2Bricklet::new(UID, &ipcon); // Create device object.
 
     ipcon.connect((HOST, PORT)).recv()??; // Connect to brickd.
-                                          // Don't use device before ipcon is connected.
+    // Don't use device before ipcon is connected.
 
-    let uvi_receiver = uvl.get_uvi_callback_receiver();
+     let uvi_receiver = uvl.get_uvi_callback_receiver();
 
-    // Spawn thread to handle received callback messages.
-    // This thread ends when the `uvl` object
-    // is dropped, so there is no need for manual cleanup.
-    thread::spawn(move || {
-        for uvi in uvi_receiver {
-            println!("UV Index: {}", uvi as f32 / 10.0);
-            println!("UV index > 3. Use sunscreen!");
-        }
-    });
+        // Spawn thread to handle received callback messages. 
+        // This thread ends when the `uvl` object
+        // is dropped, so there is no need for manual cleanup.
+        thread::spawn(move || {
+            for uvi in uvi_receiver {           
+                		println!("UV Index: {}", uvi as f32 /10.0);
+		println!("UV index > 3. Use sunscreen!");
+            }
+        });
 
-    // Configure threshold for UV index "greater than 3"
-    // with a debounce period of 1s (1000ms).
-    uvl.set_uvi_callback_configuration(1000, false, '>', 3 * 10, 0);
+		// Configure threshold for UV index "greater than 3"
+		// with a debounce period of 1s (1000ms).
+		uvl.set_uvi_callback_configuration(1000, false, '>', 3*10, 0);
 
     println!("Press enter to exit.");
     let mut _input = String::new();
